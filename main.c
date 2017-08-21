32fae2fa07de0863b0d0fda2a73a8f717c6ef0a7
Haidyn Mcleod <haidyn1988@gmail.com>
2017-08-21 15:40:46 +1000

    Feat working multicast

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
-/* A simple server in the internet domain using TCP.
-Server.cD. Thiebaut
+/*
 Adapted from http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
 The port number used in 51717.
 This code is compiled and run on the Raspberry as follows:
@@ -13,72 +12,126 @@ If the server receives -1 it closes the socket with the client.
 If the server receives -2, it exits.
 */
 
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
+#include <arpa/inet.h>
 #include <time.h>
+#include <string.h>
+#include <stdio.h>
 
-#include "network.c"
 #include "turntable.c"
 
-//#define h_addr h_addr_list[0] /* uncomment if an error for backwards compatability */
+#define HELLO_PORT 50122
+#define HELLO_GROUP "239.0.0.1" // Multicast group IP address
+#define MSGBUFSIZE 256
+#define local_address "169.254.17.247" // ethernet IP address
 
-const char serverIp[] = "169.254.17.24";
-const int portno = 51717;
+struct Tuple {
+    int socket;
+    struct sockaddr_in address;
+};
 
-int func( int a ) {
-   return 2 * a;
+struct Tuple initMulticast(){
+    struct in_addr localInterface;
+    struct sockaddr_in groupSock;
+    struct ip_mreq group;
+    int sd;
+
+    // Create a datagram socket on which to send.
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sd < 0) {
+      perror("Opening datagram socket error");
+      exit(1);
+    }
+
+    // Initialize the group sockaddr structure with a
+    memset((char *) &groupSock, 0, sizeof(groupSock));
+    groupSock.sin_family = AF_INET;
+    groupSock.sin_addr.s_addr = inet_addr(HELLO_GROUP);
+    groupSock.sin_port = htons(HELLO_PORT);
+
+    // Disable loopback so you do not receive your own datagrams.
+    char loopch = 0;
+    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch)) < 0) {
+        perror("Setting IP_MULTICAST_LOOP error");
+        close(sd);
+        exit(1);
+    }
+
+    // Set local interface for outbound multicast datagrams.
+    // The IP address specified must be associated with a local,
+    // multicast capable interface.
+    localInterface.s_addr = inet_addr(local_address);
+    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0) {
+      perror("Setting local interface error");
+      exit(1);
+    }
+
+    // Enable SO_REUSEADDR to allow multiple instances of this
+    // application to receive copies of the multicast datagrams.
+    int reuse = 1;
+    if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
+        perror("Setting SO_REUSEADDR error");
+        close(sd);
+        exit(1);
+    }
+
+    // Bind to the proper port number with the IP address
+    if(bind(sd, (struct sockaddr*)&groupSock, sizeof(groupSock))) {
+        perror("Binding datagram socket error");
+        close(sd);
+        exit(1);
+    }
+
+    // Join the multicast group on the local IP4
+    // interface. Note that this IP_ADD_MEMBERSHIP option must be
+    // called for each local interface over which the multicast
+    // datagrams are to be received.
+    group.imr_multiaddr.s_addr = inet_addr(HELLO_GROUP);
+    group.imr_interface.s_addr = inet_addr((char*) local_address);
+
+    if(setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0) {
+        perror("Adding multicast group error");
+        close(sd);
+        exit(1);
+    }
+
+    struct Tuple r = {sd, groupSock};
+    return r;
 }
 
-int main(int argc, char *argv[]) {
-    int sockfd=0, n;
-    //struct sockaddr_in serv_addr;
-    //struct hostent *server;
-    char buffer[256];
-    int data;
-    time_t start, end;
-    double diff = 0;
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in groupSock;
+    int sd;
+    char databuf_out[1024] = "Multicast test message lol!";
+    int datalen_out = sizeof(databuf_out);
+    char databuf_in[1024];
+    int datalen_in = sizeof(databuf_in);
 
-    while(1){
-        time(&start);
+    // iniatalise the multicast send and receive socket
+    struct Tuple tup = initMulticast(&sd);
+    sd = tup.socket;
+    groupSock = tup.address;
 
-        // connect to the server
-        if(sockfd == 0){
-            sockfd = connectServer(argc, serverIp, portno);
-            //send a connect request to the server
-            sendData( sockfd, 1.0 );
+    while (1){
+        // Read from the socket.
+        if(read(sd, databuf_in, datalen_in) < 0) {
+            perror("Reading datagram message error");
+            close(sd);
+            exit(1);
+        } else {
+            printf("Reading datagram message...OK.\n");
+            printf("The message from multicast server is: \"%s\"\n", databuf_in);
         }
 
-        // retreive data from the server
-        data = getData( sockfd );
-        printf("%d ->  %d\n",n, data );
-        // check if the turntable needs to perform an action
-        if(data == 3){
-            //perform motor movement
-            int ack = spinMotor(45);
-            sleep(2);
-            sendData( sockfd, ack );
-        }else{
-            sendData( sockfd, 0 );
+        if(sendto(sd, databuf_out, datalen_out, 0, (struct sockaddr*)&groupSock, sizeof(groupSock)) < 0) {
+                perror("Sending datagram message error");
+        } else {
+          printf("Sending datagram message...OK\n");
         }
-
-        while(diff < 1.0){
-            time(&end);
-            diff = difftime(end, start);
-            //printf("diff %d\n", diff);
-        }
-        diff = 0;
-
-        // close the socket when done
-        //sendData(sockfd, -2);
-        //close(sockfd);
     }
+
     return 0;
 }
-
