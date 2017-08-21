fee4f78dedf1f1555413731057ce28bfa23f8ed3
Haidyn Mcleod <haidyn1988@gmail.com>
2017-08-21 16:11:49 +1000

    Feat multicast in seperate file

--- main.c
+++ main.c
@@ -12,102 +12,20 @@ If the server receives -1 it closes the socket with the client.
 If the server receives -2, it exits.
 */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <time.h>
-#include <string.h>
 #include <stdio.h>
 
 #include "turntable.c"
+#include "network.c"
 
-#define HELLO_PORT 50122
-#define HELLO_GROUP "239.0.0.1" // Multicast group IP address
 #define MSGBUFSIZE 256
-#define local_address "169.254.17.247" // ethernet IP address
-
-struct Tuple {
-    int socket;
-    struct sockaddr_in address;
-};
-
-struct Tuple initMulticast(){
-    struct in_addr localInterface;
-    struct sockaddr_in groupSock;
-    struct ip_mreq group;
-    int sd;
-
-    // Create a datagram socket on which to send.
-    sd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sd < 0) {
-      perror("Opening datagram socket error");
-      exit(1);
-    }
-
-    // Initialize the group sockaddr structure with a
-    memset((char *) &groupSock, 0, sizeof(groupSock));
-    groupSock.sin_family = AF_INET;
-    groupSock.sin_addr.s_addr = inet_addr(HELLO_GROUP);
-    groupSock.sin_port = htons(HELLO_PORT);
-
-    // Disable loopback so you do not receive your own datagrams.
-    char loopch = 0;
-    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch)) < 0) {
-        perror("Setting IP_MULTICAST_LOOP error");
-        close(sd);
-        exit(1);
-    }
-
-    // Set local interface for outbound multicast datagrams.
-    // The IP address specified must be associated with a local,
-    // multicast capable interface.
-    localInterface.s_addr = inet_addr(local_address);
-    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0) {
-      perror("Setting local interface error");
-      exit(1);
-    }
-
-    // Enable SO_REUSEADDR to allow multiple instances of this
-    // application to receive copies of the multicast datagrams.
-    int reuse = 1;
-    if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
-        perror("Setting SO_REUSEADDR error");
-        close(sd);
-        exit(1);
-    }
-
-    // Bind to the proper port number with the IP address
-    if(bind(sd, (struct sockaddr*)&groupSock, sizeof(groupSock))) {
-        perror("Binding datagram socket error");
-        close(sd);
-        exit(1);
-    }
-
-    // Join the multicast group on the local IP4
-    // interface. Note that this IP_ADD_MEMBERSHIP option must be
-    // called for each local interface over which the multicast
-    // datagrams are to be received.
-    group.imr_multiaddr.s_addr = inet_addr(HELLO_GROUP);
-    group.imr_interface.s_addr = inet_addr((char*) local_address);
-
-    if(setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0) {
-        perror("Adding multicast group error");
-        close(sd);
-        exit(1);
-    }
-
-    struct Tuple r = {sd, groupSock};
-    return r;
-}
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in groupSock;
     int sd;
-    char databuf_out[1024] = "Multicast test message lol!";
+    char databuf_out[MSGBUFSIZE] = "Multicast test message lol!";
     int datalen_out = sizeof(databuf_out);
-    char databuf_in[1024];
+    char databuf_in[MSGBUFSIZE];
     int datalen_in = sizeof(databuf_in);
 
     // iniatalise the multicast send and receive socket
@@ -128,8 +46,6 @@ int main(int argc, char *argv[])
 
         if(sendto(sd, databuf_out, datalen_out, 0, (struct sockaddr*)&groupSock, sizeof(groupSock)) < 0) {
                 perror("Sending datagram message error");
-        } else {
-          printf("Sending datagram message...OK\n");
         }
     }
 
--- network.c
+++ network.c
@@ -1,55 +1,85 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
 
-void error(const char *msg ) {
-    perror(  msg );
-    exit(1);
-}
+#define PORT_NUM 50122
+#define GROUP_ADDRESS "239.0.0.1" // Multicast group IP address
+#define LOCAL_ADDRESS "169.254.17.247" // ethernet IP address
 
-void sendData(const int sockfd, const int x ) {
-    int n;
+struct Tuple {
+    int socket;
+    struct sockaddr_in address;
+};
 
-    char buffer[32];
-    sprintf( buffer, "%d\n", x );
-    if ( (n = write( sockfd, buffer, strlen(buffer) ) ) < 0 ){
-        error("ERROR writing to socket");
-    }
-    buffer[n] = '\0';
-}
+struct Tuple initMulticast(){
+    struct in_addr localInterface;
+    struct sockaddr_in groupSock;
+    struct ip_mreq group;
+    int sd;
 
-int getData(const int sockfd ) {
-    char buffer[32];
-    int n;
-
-    if ( (n = read(sockfd,buffer,31) ) < 0 ) {
-        error("ERROR reading from socket");
-    }
-    buffer[n] = '\0';
-    return atoi( buffer );
-}
-
-int connectServer(const int argc, const char serverIp[], const int portno){
-    int sockfd;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    if (argc < 3) {
-        // error( const_cast<char *>( "usage myClient2 hostname port\n" ) );
-        printf( "contacting %s on port %d\n", serverIp, portno );
-        // exit(0);
-    }
-    if ( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ){
-        error("ERROR opening socket");
-    }
-    if ( ( server = gethostbyname( serverIp ) ) == NULL ){
-        error("ERROR, no such host\n");
+    // Create a datagram socket on which to send.
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sd < 0) {
+      perror("Opening datagram socket error");
+      exit(1);
     }
 
-    bzero( (char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy( (char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0){
-        error("ERROR connecting");
+    // Initialize the group sockaddr structure with a
+    memset((char *) &groupSock, 0, sizeof(groupSock));
+    groupSock.sin_family = AF_INET;
+    groupSock.sin_addr.s_addr = inet_addr(GROUP_ADDRESS);
+    groupSock.sin_port = htons(PORT_NUM);
+
+    // Disable loopback so you do not receive your own datagrams.
+    char loopch = 0;
+    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch)) < 0) {
+        perror("Setting IP_MULTICAST_LOOP error");
+        close(sd);
+        exit(1);
     }
 
-    return sockfd;
+    // Set local interface for outbound multicast datagrams.
+    // The IP address specified must be associated with a local,
+    // multicast capable interface.
+    localInterface.s_addr = inet_addr(LOCAL_ADDRESS);
+    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0) {
+      perror("Setting local interface error");
+      exit(1);
+    }
+
+    // Enable SO_REUSEADDR to allow multiple instances of this
+    // application to receive copies of the multicast datagrams.
+    int reuse = 1;
+    if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
+        perror("Setting SO_REUSEADDR error");
+        close(sd);
+        exit(1);
+    }
+
+    // Bind to the proper port number with the IP address
+    if(bind(sd, (struct sockaddr*)&groupSock, sizeof(groupSock))) {
+        perror("Binding datagram socket error");
+        close(sd);
+        exit(1);
+    }
+
+    // Join the multicast group on the local IP4
+    // interface. Note that this IP_ADD_MEMBERSHIP option must be
+    // called for each local interface over which the multicast
+    // datagrams are to be received.
+    group.imr_multiaddr.s_addr = inet_addr(GROUP_ADDRESS);
+    group.imr_interface.s_addr = inet_addr((char*) LOCAL_ADDRESS);
+
+    if(setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0) {
+        perror("Adding multicast group error");
+        close(sd);
+        exit(1);
+    }
+
+    struct Tuple r = {sd, groupSock};
+    return r;
 }
