d58c434b77ff587462d8d4780757927d3abca537
Haidyn McLeod <haidyn.mcleod@gmail.com>
2017-09-06 10:54:41 +1000

    Feat: update variable names and comments

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 /*
-Adapted from http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
-The port number used in 51717.
+This file is adapted from http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html and 
+http://www.tenouk.com/Module41c.html. 
+
+Creates both a server and client multicast network for embedded Linux devices such as the Raspberry Pi, 
+Intel Edison or BeagleBone using Wifi or ethernet.     
+
 This code is compiled and run on the Raspberry as follows:
 
-    g++ -o turntable main.c network.c turntable.c -lwiringPi -lwiringPiDev
-    ./turntable
+    g++ -o multicast main.c
+    ./multicast
 
-The server waits for a connection request from a client.
-The server assumes the client will send positive integers, which it sends back multiplied by 2.
-If the server receives -1 it closes the socket with the client.
-If the server receives -2, it exits.
-
-http://www.tenouk.com/Module41c.html
+The system waits for data to be received from the network, prints the received data and then responds by 
+sendding a string back to the network.
 */
 
 #include <sys/types.h>
@@ -22,10 +22,10 @@ http://www.tenouk.com/Module41c.html
 #include <string.h>
 #include <stdio.h>
 
-#define HELLO_PORT 50122
-#define HELLO_GROUP "239.0.0.1" // Multicast group IP address
-#define MSGBUFSIZE 256
-#define local_address "169.254.17.247" // ethernet IP address
+#define PORT 50122      // Port number to listen and send data on
+#define GROUP_IP "239.0.0.1" // Multicast group IP address
+#define MSGBUFSIZE 256      // size of the send and receive data buffers
+#define LOCAL_IP "169.254.17.247" // IP address that interfaces with the network, Ethernet or Wifi
 
 struct Tuple {
     int socket;
@@ -48,8 +48,8 @@ struct Tuple initMulticast(){
     // Initialize the group sockaddr structure with a
     memset((char *) &groupSock, 0, sizeof(groupSock));
     groupSock.sin_family = AF_INET;
-    groupSock.sin_addr.s_addr = inet_addr(HELLO_GROUP);
-    groupSock.sin_port = htons(HELLO_PORT);
+    groupSock.sin_addr.s_addr = inet_addr(GROUP_IP);
+    groupSock.sin_port = htons(PORT);
 
     // Disable loopback so you do not receive your own datagrams.
     char loopch = 0;
@@ -62,7 +62,7 @@ struct Tuple initMulticast(){
     // Set local interface for outbound multicast datagrams.
     // The IP address specified must be associated with a local,
     // multicast capable interface.
-    localInterface.s_addr = inet_addr(local_address);
+    localInterface.s_addr = inet_addr(LOCAL_IP);
     if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0) {
       perror("Setting local interface error");
       exit(1);
@@ -88,8 +88,8 @@ struct Tuple initMulticast(){
     // interface. Note that this IP_ADD_MEMBERSHIP option must be
     // called for each local interface over which the multicast
     // datagrams are to be received.
-    group.imr_multiaddr.s_addr = inet_addr(HELLO_GROUP);
-    group.imr_interface.s_addr = inet_addr((char*) local_address);
+    group.imr_multiaddr.s_addr = inet_addr(GROUP_IP);
+    group.imr_interface.s_addr = inet_addr((char*) LOCAL_IP);
 
     if(setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0) {
         perror("Adding multicast group error");
@@ -106,9 +106,9 @@ int main(int argc, char *argv[])
     struct sockaddr_in groupSock;
     struct sockaddr_in addr;
     int sd;
-    char databuf_out[1024] = "Multicast test message lol!";
+    char databuf_out[MSGBUFSIZE] = "Multicast test message lol!";
     int datalen_out = sizeof(databuf_out);
-    char databuf_in[1024];
+    char databuf_in[MSGBUFSIZE];
     int datalen_in = sizeof(databuf_in);
 
     // iniatalise the multicast send and receive socket
@@ -117,11 +117,12 @@ int main(int argc, char *argv[])
     groupSock = tup.address;
 
     while (1){
-        // Read from the socket.
-        int nbytes=recvfrom(sd,&databuf_in,MSGBUFSIZE,0, (struct sockaddr *) &groupSock,sizeof(groupSock));
+        // wait here until data is recieved and read from the socket. 
+        int nbytes=recvfrom(sd,&databuf_in,datalen_in,0, (struct sockaddr*)&groupSock,sizeof(groupSock));
 
-        printf("\n");
+        printf("Bytes Received: %d.  The message is: %s\n", nbytes, databuf_in);
 
+        // send data to the network
         if(sendto(sd, databuf_out, datalen_out, 0, (struct sockaddr*)&groupSock, sizeof(groupSock)) < 0) {
                 perror("Sending datagram message error");
         } else {
